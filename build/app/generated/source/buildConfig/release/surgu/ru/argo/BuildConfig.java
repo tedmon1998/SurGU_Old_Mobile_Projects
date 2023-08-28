d6e62b99c990a2bb6896d9231d491eb583410d3d
Абабий Виталий Дмитриевич <ababij_vd@surgu.ru>
2023-08-28 15:13:59 +0500

    add old surgu mobile project

---
+++ build/app/generated/source/buildConfig/release/surgu/ru/argo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package surgu.ru.argo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "surgu.ru.argo";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
